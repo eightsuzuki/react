0e9fb660ef46c9a9f879e3bff3ac9702cc3f5624
eightsuzuki <113869804+eightsuzuki@users.noreply.github.com>
2023-05-12 23:47:34 +0900

    Add files via upload

--- packages/react-dom-bindings/src/events/__tests__/ReactDOMEventListener-test.js
+++ packages/react-dom-bindings/src/events/__tests__/ReactDOMEventListener-test.js
@@ -1,33 +1,5 @@
-import {JSDOM} from 'jsdom';
-
-import { 
-  dispatchDiscreteEvent,
-  getEventPriority 
-} from '../ReactDOMEventListener';
+import { getEventPriority } from '../ReactDOMEventListener';
 
 test('call test', () => {
   getEventPriority('message');
 });
-
-test('dispathEvent tests',() => {
-  const domEventName = 'click';
-  const eventSystemFlags = { bubbles: true};
-  // const container = document.createElement('div');
-  const nativeEvent = {type: 'click'};
-  const dom = new JSDOM('<!DCTYPE html><html><body></body></html>');
-  const { window }  = dom;
-  const { document } = window;
-
-  const myButton =document.createElement('buttton');
-  myButton.textContent ='Click me';
-
-  document.body.appendChild(myButton);
-
-  myButton.addEventListener('click', () =>{
-    console.log('Button clicked!');  
-  });
-
-  dispatchDiscreteEvent(domEventName, eventSystemFlags, container, nativeEvent);
-});
-
-
